59ff36609155f95f2efa798cfcd384ef8f33419c
Adrian Hafizh <atharfazle202@gmail.com>
2023-05-06 13:28:22 +0700

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import {Link} from 'react-router-dom'
+import Logo from '../assets/img/logo.svg'
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <Link to="/">
+          <img src={Logo} alt=""/>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
